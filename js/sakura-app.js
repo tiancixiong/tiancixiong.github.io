c40787f122d47990954fad86d8ce9356d6f6d9f4
tiancixiong <tiancixiong@163.com>
2019-06-28 16:25:43 +0800

    Site updated: 2019-06-28 16:25:40

--- js/sakura-app.js
+++ js/sakura-app.js
@@ -698,7 +698,7 @@ var pjaxInit = function () {
   smileBoxToggle()
   timeSeriesReload()
   add_copyright()
-  //console.log($('#myscript').text())
+  console.log($('#myscript').text())
 }
 $(document).on('click', '.sm', function () {
   var msg = '您真的要设为私密吗？'
@@ -1108,8 +1108,7 @@ var home = location.href,
     },
     MJ: function () {
       if (mashiro_option.mathjax == '1') {
-        //$.getScript('//cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.4/MathJax.js?config=TeX-MML-AM_CHTML', function () {
-		$.getScript('https://cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.4/MathJax.js?config=TeX-MML-AM_CHTML', function () {
+        $.getScript('//cdnjs.cloudflare.com/ajax/libs/mathjax/2.7.4/MathJax.js?config=TeX-MML-AM_CHTML', function () {
           MathJax.Hub.Config({tex2jax: {inlineMath: [['$', '$'], ['\\(', '\\)']]}})
           var math = document.getElementsByClassName('entry-content')[0]
           MathJax.Hub.Queue(['Typeset', MathJax.Hub, math])
